c70aa26a4eb40c9468633e926816b854f33f3d46
Nika <nika.zupancic05@gmail.com>
2024-11-27 20:17:24 +0100

    hello world from phaser

--- game2048/src/App.js
+++ game2048/src/App.js
@@ -1,12 +1,13 @@
-import './App.css';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        Hallo
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
