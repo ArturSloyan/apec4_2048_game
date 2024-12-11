c0aca64b1d24b1c695635ec6e575b026ff94c091
Hilal Karsli <hilalkarsli@icloud.com>
2024-12-11 15:27:17 +0100

    resolved conversations in PR comments

--- game2048/dbconnection/bcrypt-password-hash/passwordHashing.cjs
+++ game2048/dbconnection/bcrypt-password-hash/passwordHashing.cjs
@@ -7,4 +7,27 @@ async function saltHashPassword(userPassword) {
     return hashedPassword;
 }
 
-module.exports = { saltHashPassword }; // export, so it can be used in other documents
+// storedHashedPassword represents the hashed password retrieved from database
+// userInputPasswort is the plaintext password provided by the user during login
+// the bcrypt.compare() function compares these two passwords and returns a boolean value indicating wheter they match
+
+const userInputPassword = '';
+const storedHashedPassword = '';
+bcrypt.compare(userInputPassword, storedHashedPassword, (err, result) => {
+    if (err) {
+        // Handle error
+        console.error('Error comparing passwords:', err);
+        return;
+    }
+
+if (result) {
+    // Passwords match, authentication successful
+    console.log('Passwords match! User authenticated.');
+} else {
+    // Passwords don't match, authentication failed
+    console.log('Passwords do not match! Authentication failed.');
+}
+});
+
+// export, so it can be used in other documents
+module.exports = { saltHashPassword };
