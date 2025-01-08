ec4bbf1bf9cd2cc3031cecffcfad925718298ae4
Nika <nika.zupancic05@gmail.com>
2025-01-08 14:23:07 +0100

    in progress

--- game2048/src/components/scenes/Grid.js
+++ game2048/src/components/scenes/Grid.js
@@ -1,36 +1,37 @@
 import Phaser from "phaser";
 
 export class Grid extends Phaser.Scene {
-  imageOne;
   cursors;
+  imageOne;
+  imageTwo;
 
   preload() {
     this.load.image("two", "./assets/2.svg");
   }
 
   create() {
-    this.imageOne = this.physics.add.image(500, 200, "two");
+    this.imageOne = this.physics.add.image(0, 0, "two");
     this.imageOne.setCollideWorldBounds(true);
 
     this.cursors = this.input.keyboard.createCursorKeys();
   }
 
   update() {
+
+    
+
     if (this.cursors.left.isDown) {
       // rectangle move left
-      this.imageOne.setVelocityX(-100);
+      this.physics.moveTo(this.imageOne, 0, 0);
     } else if (this.cursors.right.isDown) {
       // rectangle move right
-      this.imageOne.setVelocityX(100);
+      this.physics.moveTo(this.imageOne, 400, 0);
     } else if (this.cursors.up.isDown) {
       // rectangle move up
-      this.imageOne.setVelocityY(-100);
+      this.physics.moveTo(this.imageOne, 0, 100);
     } else if (this.cursors.down.isDown) {
       // rectangle move down {
-      this.imageOne.setVelocityY(100);
-    } else {
-      this.imageOne.setVelocityY(0);
-      this.imageOne.setVelocityX(0);
+      this.physics.moveTo(this.imageOne, 0, 100);
     }
   }
 }
